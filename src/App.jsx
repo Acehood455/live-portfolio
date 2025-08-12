4f69bdf25614d6a90bc1e41c40df8b7978e5a0b2
Your Name <you@example.com>
2025-08-12 16:53:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BackgroundVideo from './components/BackgroundVideo'
+import Navbar from './components/NavBar'
+
+const App = () => {
+  return (
+    <div>
+      {/* <BackgroundVideo /> */}
+    {/* <div className="container path mt-20 ml-20"></div> */}
+    {/* <div className="container2 path2 mt-20 ml-20"></div> */}
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
