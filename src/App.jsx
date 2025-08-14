cb05f92a80ce6a1e6a057ba565289b065491ca48
Acehood455 <acetaiwo8@gmail.com>
2025-08-14 22:01:57 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import BackgroundVideo from './components/BackgroundVideo'
 import Navbar from './components/NavBar'
 import Hero from './section/Hero'
+import About from './section/About'
 
 const App = () => {
   return (
-    <div>
+    <div className=''>
       {/* <BackgroundVideo /> */}
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
+      <About />
     </div>
   )
 }
